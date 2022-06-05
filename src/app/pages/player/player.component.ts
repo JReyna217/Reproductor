9df9dadbb20e7159fe25e2e00024f9e66526bac6
Julio Reyna <julioreyna_morisco@hotmail.com>
2022-06-05 17:38:42 -0600

    Diseño y métodos base

--- src/app/pages/player/player.component.ts
+++ src/app/pages/player/player.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent implements OnInit {
+export class PlayerComponent {
+
+  canciones: Array<any> = [];
+  state: any;
+
+  caratulaCancionActual: string = "http://cdn.onlinewebfonts.com/svg/img_296254.png";
+  nombreCancionActual: string = "Reproduce una canción";
+
+  cancionActual: any = {};
 
   constructor() { }
 
-  ngOnInit(): void {
+  playStream(url: string) {
+
   }
 
+  openFile(file: any, index: number) {
+
+  }
+
+  pause() {
+
+  }
+
+  play() {
+
+  }
+
+  stop() {
+
+  }
+
+  next() {
+
+  }
+
+  previous() {
+
+  }
+
+  isFirstPlaying() {
+
+  }
+
+  isLastPlaying() {
+
+  }
+
+  onSliderChangeEnd(change: any) {
+
+  }
 }
